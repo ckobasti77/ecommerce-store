e333f15ccbd3ea2c72671dd654198598b81d9e7f
Jovan Milojevic <jovanm028@gmail.com>
2023-11-16 23:29:04 -0800

    sta mu je

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import Container from "@/components/ui/container";
-import useCart from "@/hooks/use-cart";
-import CartItem from "./components/cart-item";
-import Summary from "./components/summary";
+// import useCart from "@/hooks/use-cart";
+// import CartItem from "./components/cart-item";
+// import Summary from "./components/summary";
 
 const CartPage = () => {
-  const cart = useCart();
+  // const cart = useCart();
 
   return (
     <div className="bg-white">
@@ -24,7 +24,7 @@ const CartPage = () => {
                 ))} */}
               </ul>
             </div>
-            <Summary />
+            {/* <Summary /> */}
           </div>
         </div>
       </Container>
