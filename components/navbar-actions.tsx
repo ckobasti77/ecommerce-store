3410e3c69db72bc019ac15e900dfecbc632e6911
Jovan Milojevic <jovanm028@gmail.com>
2023-11-16 23:58:19 -0800

    comm

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -22,12 +22,12 @@ const NavbarActions = () => {
   
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black py-2">
+      {/* <Button disabled onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black py-2">
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
         </span>
-      </Button>
+      </Button> */}
     </div>
   );
 };
